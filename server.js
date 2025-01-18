7e916337c5f3dfbc4d5962de75fc67af45cace62
Superior5 <superiorunknown5@gmail.com>
2025-01-18 17:21:33 +0300

    fix

--- server.js
+++ server.js
@@ -31,47 +31,53 @@ app.use(cookieParser())
 const users = [];
 
 app.use('/api/v1/api-docs', basicAuth({
-    users: { 'admin': '1234567' }, // Укажите имя пользователя и пароль
-    challenge: true, // Включить стандартное окно запроса логина/пароля
-  }), swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+  users: { 'admin': '1234567' }, // Укажите имя пользователя и пароль
+  challenge: true, // Включить стандартное окно запроса логина/пароля
+}), swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 app.use('/api/v1/guides', guideRoutes);
 app.use('/api/v1/routes', routeRoutes);
 app.use('/api/v1/admin', systemRoutes);
 app.use('/api/v1/auth', authRoutes);
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: '*', // Для разработки. Настройте для продакшена.
+    methods: ['GET', 'POST'],
+  },
+  path: '/my-socket'
+});
 
 io.on('connection', (socket) => {
-    console.log('Новое соединение:', socket.id);
-  
-    // Авторизация
-    socket.on('authenticate', ({ userId, adminId }) => {
-      users[userId] = { socketId: socket.id, adminId };
-      console.log(`Пользователь ${userId} связан с администратором ${adminId}`);
-    });
-  
-    // Прием геоданных от гида
-    socket.on('send-location', ({ userId, location }) => {
+  console.log('Новое соединение:', socket.id);
+
+  // Авторизация
+  socket.on('authenticate', ({ userId, adminId }) => {
+    users[userId] = { socketId: socket.id, adminId };
+    console.log(`Пользователь ${userId} связан с администратором ${adminId}`);
+  });
+
+  // Прием геоданных от гида
+  socket.on('send-location', ({ userId, location }) => {
     //   const admin = Object.values(users).find((user) => user.userId === users[userId].adminId);
-      console.log(userId, location)
+    console.log(userId, location)
     //   if (admin) {
     //     io.to(admin.socketId).emit('receive-location', { userId, location });
     //   }
-    });
-  
-    // Обработчик отключения
-    socket.on('disconnect', () => {
-      for (const userId in users) {
-        if (users[userId].socketId === socket.id) {
-          console.log(`Пользователь ${userId} отключен`);
-          delete users[userId];
-          break;
-        }
-      }
-    });
   });
 
-app.listen(5000, ()=> {
-    console.log('сервер запущен');  
+  // Обработчик отключения
+  socket.on('disconnect', () => {
+    for (const userId in users) {
+      if (users[userId].socketId === socket.id) {
+        console.log(`Пользователь ${userId} отключен`);
+        delete users[userId];
+        break;
+      }
+    }
+  });
+});
+
+server.listen(5000, () => {
+  console.log('сервер запущен');
 })
